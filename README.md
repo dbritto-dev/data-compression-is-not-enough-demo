09f7c42f8da6e912dcd9dd6460634fe61b787e65
Danilo Britto <dbritto.dev@gmail.com>
2024-09-16 10:46:19 -0500

    Minor changes

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 | Compact + Lean                      | 11.9mb | -7.20mb  |                |           |
 | Compact + Lean + Normalize          | 5.1mb  | -14.00mb | ~29sec         | ~1.7min   |
 | Compact + Lean + Normalize + Gzip   | 429kb  | -18.67mb | ~3sec          | ~11sec    |
-| Compact + Lean + Normalize + Brotli | 70.5kb | -19.03mb | ~1sec          | ~3sec     |
+| Compact + Lean + Normalize + Brotli | 69kb   | -19.03mb | ~1sec          | ~3sec     |
 | Gzip                                | 705kb  | -18.04mb | ~13sec         | ~16sec    |
 | Brotli                              | 90.2kb | -19.01mb | ~3sec          | ~4sec     |
--- main.py
+++ main.py
@@ -78,10 +78,15 @@ def normalize_data(items: list[dict]) -> dict:
 
     event = None
     listings = []
+    ticket_type_ids = {}
     ticket_types = {}
+    provider_ids = {}
     providers = {}
+    section_ids = {}
     sections = {}
+    block_ids = {}
     blocks = {}
+    row_ids = {}
     rows = {}
     for item in items:
         normalized_listing = {
@@ -89,15 +94,15 @@ def normalize_data(items: list[dict]) -> dict:
             "quantity": item["quantity"],
             "deep_link": item["deepLink"],
             "price": item.get("sellValue", 0),
-            "final_rice": item.get("sellValue", 0) + item.get("totalFee", 0),
+            "final_price": item.get("sellValue", 0) + item.get("totalFee", 0),
         }
 
         if not event:
             event = {"id": item["eventId"], "slug": item["eventSlug"]}
 
-        category_blocks = item.get("catBlocks")
+        category_blocks = item.get("categoryBlocks")
         if category_blocks:
-            normalized_listing["category_blocks"] = category_blocks
+            normalized_listing["category_blocks"] = unique_list(category_blocks)
 
         seating_quantity = item.get("seatingQty")
         if seating_quantity:
@@ -106,8 +111,12 @@ def normalize_data(items: list[dict]) -> dict:
         ticket_type = item.get("ticketType")
         ticket_type_id = None
         if ticket_type:
-            ticket_type_id = slugify(ticket_type)
-            if ticket_type_id not in ticket_types:
+            ticket_type_id_key = slugify(ticket_type)
+            ticket_type_id = ticket_type_ids.get(ticket_type_id_key)
+            if not ticket_type_id:
+                ticket_type_id = len(ticket_type_ids.keys()) + 1
+                ticket_type_ids[ticket_type_id_key] = ticket_type_id
+            if ticket_type_id and ticket_type_id not in ticket_types:
                 ticket_types[ticket_type_id] = {
                     "id": ticket_type_id,
                     "name": ticket_type,
@@ -117,8 +126,12 @@ def normalize_data(items: list[dict]) -> dict:
         provider = item.get("providerName")
         provider_id = None
         if provider:
-            provider_id = slugify(provider)
-            if provider_id not in providers:
+            provider_id_key = slugify(provider)
+            provider_id = provider_ids.get(provider_id_key)
+            if not provider_id:
+                provider_id = len(provider_ids.keys()) + 1
+                provider_ids[provider_id_key] = provider_id
+            if provider_id and provider_id not in providers:
                 providers[provider_id] = {
                     "id": provider_id,
                     "name": provider,
@@ -128,8 +141,12 @@ def normalize_data(items: list[dict]) -> dict:
         section = item.get("section")
         section_id = None
         if section:
-            section_id = slugify(section)
-            if section_id not in sections:
+            section_id_key = slugify(section)
+            section_id = section_ids.get(section_id_key)
+            if not section_id:
+                section_id = len(section_ids.keys()) + 1
+                section_ids[section_id_key] = section_id
+            if section_id and section_id not in sections:
                 sections[section_id] = {
                     "id": section_id,
                     "name": section,
@@ -139,8 +156,12 @@ def normalize_data(items: list[dict]) -> dict:
         block = item.get("block")
         block_id = None
         if block:
-            block_id = slugify(block)
-            if block_id not in blocks:
+            block_id_key = slugify(block)
+            block_id = block_ids.get(block_id_key)
+            if not block_id:
+                block_id = len(block_ids.keys()) + 1
+                block_ids[block_id_key] = block_id
+            if block_id and block_id not in blocks:
                 blocks[block_id] = {
                     "id": block_id,
                     "name": block,
@@ -150,8 +171,12 @@ def normalize_data(items: list[dict]) -> dict:
         row = item.get("row")
         row_id = None
         if row:
-            row_id = slugify(row)
-            if row_id not in rows:
+            row_id_key = slugify(row)
+            row_id = row_ids.get(row_id_key)
+            if not row_id:
+                row_id = len(row_ids.keys()) + 1
+                row_ids[row_id_key] = row_id
+            if row_id and row_id not in rows:
                 rows[row_id] = {
                     "id": row_id,
                     "name": row,
@@ -177,3 +202,7 @@ def slugify(value: str) -> str:
     )
     value = re.sub(r"[^\w\s-]", "", value.lower())
     return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+
+def unique_list(values: list) -> list:
+    return list(set(values))
